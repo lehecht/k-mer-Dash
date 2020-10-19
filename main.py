d48aa1df5ea4f267adf1f43a0aa27a8e00dc8cc4
Leane <lhecht@techfak.uni-bielefeld.de>
2020-10-19 19:52:32 +0200

    command line arguments can be read refs #3

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from argparse import ArgumentParser
+
+argparser = ArgumentParser()
+argparser.add_argument('-f', '--files', dest='data', action='store', required=True)
+argparser.add_argument('-k', dest='k', action='store', type=int, required=True)
+argparser.add_argument('-p', '--peak', dest='peak', nargs='?', action='store', type=int)
+argparser.add_argument('-t', '--top', dest='top', default=10, nargs='?', action='store', type=int)
+argparser.add_argument('-c', '--console', dest='console', default=False, nargs='?', action='store', type=bool)
+
 if __name__ == '__main__':
-    pass
+    args = argparser.parse_args()
+    files = args.data.split(',')
+    for file in files:
+        try:
+            open(file)
+        except IOError:
+            print('{} does not exist'.format(file))
+            break
