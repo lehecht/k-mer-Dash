b57d59c7b4a150a05b781ebcf1544f91ec95e042
Leane <lhecht@techfak.uni-bielefeld.de>
2020-11-19 19:16:58 +0100

    adapted top-kmere highlights to changes in top-kmere function refs #16

--- src/kMerScatterPlotData.py
+++ src/kMerScatterPlotData.py
@@ -4,13 +4,15 @@ import pandas as pd
 
 class KMerScatterPlotData(Processing):
 
-    def __init__(self, data, selected, k, peak, top):
-        super().__init__(data, selected, k, peak, top)
+    def __init__(self, data, selected, k, peak, top, highlight):
+        super().__init__(data, selected, k, peak, top, highlight)
 
     def processData(self):
         top_kmer_df = self.getTopKmer()  # get top kmeres
         fileName1 = top_kmer_df['File'].drop_duplicates().tolist()[0]  # get column names
         fileName2 = top_kmer_df['File'].drop_duplicates().tolist()[1]
+
+        highlights = self.getSettings().getHighlight()
 
         xAxis = self.getDF()[fileName1].tolist()
         yAxis = self.getDF()[fileName2].tolist()
@@ -20,8 +22,17 @@ class KMerScatterPlotData(Processing):
         result_df[fileName2] = yAxis
         result_df['highlight'] = False  # highlights top kmere
 
-        kmerList = top_kmer_df.index.tolist()
-        for kmer in kmerList:
-            result_df.loc[kmer, 'highlight'] = True
+        top_kmer_df = top_kmer_df.sort_values(by=['Frequency'],
+                                              ascending=False)  # sort df by frequency getting max entries
+
+        if len(result_df) < highlights:  # checks if highlight value is valid
+            print("Amount of highlights is greater than the amount of entries!")
+            highlights = int(len(result_df) * 0.01)
+            print("Amount of highlights was set on {}".format(highlights))
+
+        highlightKmer = top_kmer_df[:highlights].index.tolist()  # collect max-kmeres for highlighting
+
+        for kmer in highlightKmer:
+            result_df.loc[kmer, ['highlight']] = True  # set highlight-entries on true for max-kmeres
 
         return [result_df, label, [fileName1, fileName2]]
