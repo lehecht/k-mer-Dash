a82814a80f9a097f0bca5db074047540ac05dabb
Leane <lhecht@techfak.uni-bielefeld.de>
2020-11-20 13:12:11 +0100

    fixed highlighting from top to max-values refs #11

--- src/kMerScatterPlotData.py
+++ src/kMerScatterPlotData.py
@@ -8,29 +8,37 @@ class KMerScatterPlotData(Processing):
         super().__init__(data, selected, k, peak, top, highlight)
 
     def processData(self):
-        top_kmer_df = self.getTopKmer()  # get top kmeres
-        fileName1 = top_kmer_df['File'].drop_duplicates().tolist()[0]  # get column names
-        fileName2 = top_kmer_df['File'].drop_duplicates().tolist()[1]
+        data = self.getDF()  # get top kmeres
+
+        fileName1 = data.columns.tolist()[0]  # get column names
+        fileName2 = data.columns.tolist()[1]
 
         highlights = self.getSettings().getHighlight()
 
-        xAxis = self.getDF()[fileName1].tolist()
-        yAxis = self.getDF()[fileName2].tolist()
-        label = self.getDF().index.tolist()
+        xAxis = data[fileName1].tolist()
+        yAxis = data[fileName2].tolist()
+        label = data.index.tolist()
 
         result_df = pd.DataFrame(xAxis, index=label, columns=[fileName1])
         result_df[fileName2] = yAxis
         result_df['highlight'] = False  # highlights top kmere
-
-        top_kmer_df = top_kmer_df.sort_values(by=['Frequency'],
-                                              ascending=False)  # sort df by frequency getting max entries
 
         if len(result_df) < highlights:  # checks if highlight value is valid
             print("Amount of highlights is greater than the amount of entries!")
             highlights = int(len(result_df) * 0.01)
             print("Amount of highlights was set on {}".format(highlights))
 
-        highlightKmer = top_kmer_df[:highlights].index.tolist()  # collect max-kmeres for highlighting
+        allFreqs = data[fileName1].values.tolist()
+        allFreqs.extend(data[fileName2].values.tolist())  # get all Frequencies
+        maxFreqs = list(set(allFreqs))  # drop dublications
+        maxFreqs.sort(reverse=True)
+        maxFreqs = maxFreqs[:highlights]
+
+        highlightKmer = []
+
+        for val in maxFreqs:
+            highlightKmer.extend(data[data[fileName1] == val].index.tolist())  # get kmeres with given Frequency
+            highlightKmer.extend(data[data[fileName2] == val].index.tolist())
 
         for kmer in highlightKmer:
             result_df.loc[kmer, ['highlight']] = True  # set highlight-entries on true for max-kmeres
