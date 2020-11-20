6c14ed55dfffa869343df6725ca2b4f746969f63
Leane <lhecht@techfak.uni-bielefeld.de>
2020-11-20 14:16:07 +0100

    adapted test to changes in kmerPcaData refs #11

--- Tests/Unit/test_kMerPCAData.py
+++ Tests/Unit/test_kMerPCAData.py
@@ -8,23 +8,24 @@ def test_processData():
     testData = ["Tests/Unit/testdata/testFile1.fa", "Tests/Unit/testdata/testFile2.fa"]
     k = 5
     top = 3
+    highlight = 0
     peak = None
-    process = Processing(testData, None, k, peak, top)
+    process = Processing(testData, None, k, peak, top, highlight)
 
     # Executing
     results = KMerPCAData.processData(process)
 
     kmer_list1 = results[0].index.tolist()
     kmer_list2 = results[1].index.tolist()
-    tFreq1 = results[4].values.tolist()
-    tFreq2 = results[5].values.tolist()
+    tFreq1 = results[4].Frequency.values.tolist()
+    tFreq2 = results[5].Frequency.values.tolist()
     fileName1 = results[2]
     fileName2 = results[3]
 
     # Testing
-    assert kmer_list1 == ['AACCC', 'ACCCC', 'AAAAA']
-    assert kmer_list2 == ['TTTGG', 'GTTGG', 'GGGGC', 'GGGCA', 'TTGGG', 'TGGGG']
+    assert kmer_list1 == ['AAACC', 'AACCC', 'ACCCC']
+    assert kmer_list2 == ['TTTGG', 'TTGGG', 'TGGGG']
     assert fileName1 == "testFile1.fa"
     assert fileName2 == "testFile2.fa"
-    assert tFreq1 == [0, 0, 0]
-    assert tFreq2 == [3, 2, 0, 0, 2, 1]
+    assert tFreq1 == [1, 2, 2]
+    assert tFreq2 == [1, 2, 3]
