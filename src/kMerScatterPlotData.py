44747764b045379c4c91de9a2fc42c05b949bb45
Leane <lhecht@techfak.uni-bielefeld.de>
2020-12-05 15:56:46 +0100

    changed layout for scatterplot refs #30

--- src/kMerScatterPlotData.py
+++ src/kMerScatterPlotData.py
@@ -1,5 +1,6 @@
 from src.processing import Processing
 import pandas as pd
+import math
 
 
 class KMerScatterPlotData(Processing):
@@ -20,9 +21,21 @@ class KMerScatterPlotData(Processing):
 
         result_df = pd.DataFrame(xAxis, index=label, columns=[fileName1])
         result_df[fileName2] = yAxis
-        result_df['highlight'] = False  # highlights top kmere
+        result_df['highlight'] = "{}-mer".format(self.getSettings().getK())  # highlights top kmere
 
         for kmer in topKmer.index.tolist():
-            result_df.loc[kmer, ['highlight']] = True  # set highlight-entries on true for max-kmeres
+            result_df.loc[kmer, ['highlight']] = "TOP {}-mer".format(
+                self.getSettings().getK())  # set highlight-entries on true for max-kmeres
 
-        return [result_df, label, [fileName1, fileName2]]
+        size_score = []
+        max_score = result_df[fileName1].max() * result_df[fileName2].max()
+        for i in range(0, len(result_df)):
+            score = result_df.iloc[i][fileName1] * result_df.iloc[i][fileName2]
+            score = score / max_score
+            if score < 0.01:
+                score = 0.01
+            size_score.append(score)
+
+        result_df["size_score"] = size_score
+
+        return [result_df, label, [fileName1, fileName2], size_score]
