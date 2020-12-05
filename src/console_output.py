c4eff0bd1a6d5fd31426ca3516a658ee8771b3fc
Leane <lhecht@techfak.uni-bielefeld.de>
2020-12-05 15:57:03 +0100

    changed layout for scatterplot refs #30

--- src/console_output.py
+++ src/console_output.py
@@ -1,5 +1,4 @@
 from src.processing import Processing
-from src.inputValueException import *
 from src.kMerScatterPlotData import KMerScatterPlotData
 from src.kMerPCAData import KMerPCAData
 from src.kMerAlignmentData import KMerAlignmentData
@@ -21,8 +20,26 @@ def printScatterPlot(process):
     df = result[0]
     label = result[1]
     fileNames = result[2]
-    fig = px.scatter(df, x=fileNames[0], y=fileNames[1], hover_name=label, color='highlight',
-                     labels={'highlight': 'Top k-Mer'}, title='Scatterplot')
+    # size_score = result[3]
+
+    fig = px.scatter(df, x=fileNames[0], y=fileNames[1], hover_name=label,
+                     color='highlight',
+                     color_discrete_map={"TOP {}-mer".format(process.getSettings().getK()): "red",
+                                         "{}-mer".format(process.getSettings().getK()): "black"},
+                     title='Scatterplot of k-Mer occurences (#)',
+                     opacity=0.55,
+                     size="size_score",
+                     hover_data={'highlight': False, fileNames[0]: True, fileNames[1]: True, 'size_score': False},
+                     )
+    fig.update_layout(
+        dict(font_color='black', legend_traceorder="reversed", legend=dict(title=None, bordercolor="Black",
+                                                                           borderwidth=1, font_size=18),
+             plot_bgcolor="white"),
+        title=dict(font_size=25, xanchor='center', yanchor='top', y=0.95, x=0.5))
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='#e6e6e6', linecolor="black",
+                     title="#k-Mer of " + fileNames[0],title_font=dict(size=18))
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='#e6e6e6', linecolor="black",
+                     title="#k-Mer of " + fileNames[1],title_font=dict(size=18))
     fig.show()
 
 
