349e3fb53aef6630dc1a7e9ebd38e76a060c8ff2
Leane <lhecht@techfak.uni-bielefeld.de>
2021-01-22 16:12:08 +0100

    optimized score-calculation refs #50

--- src/kMerScatterPlotData.py
+++ src/kMerScatterPlotData.py
@@ -1,6 +1,6 @@
 from src.processing import Processing
 import pandas as pd
-import math
+import time
 
 
 class KMerScatterPlotData(Processing):
@@ -21,21 +21,37 @@ class KMerScatterPlotData(Processing):
 
         result_df = pd.DataFrame(xAxis, index=label, columns=[fileName1])
         result_df[fileName2] = yAxis
-        result_df['highlight'] = "{}-mer".format(self.getSettings().getK())  # highlights top kmere
+
+        k = self.getSettings().getK()
+
+        result_df['highlight'] = "{}-mer".format(k)  # highlights top kmere
 
         for kmer in topKmer.index.tolist():
-            result_df.loc[kmer, ['highlight']] = "TOP {}-mer".format(
-                self.getSettings().getK())  # set highlight-entries on true for max-kmeres
+            result_df.loc[kmer, ['highlight']] = "TOP {}-mer".format(k)  # set highlight-entries on true for max-kmeres
 
-        size_score = []
         max_score = result_df[fileName1].max() * result_df[fileName2].max()
-        for i in range(0, len(result_df)):
-            score = result_df.iloc[i][fileName1] * result_df.iloc[i][fileName2]
-            score = score / max_score
-            if score < 0.01:
-                score = 0.01
-            size_score.append(score)
 
-        result_df["size_score"] = size_score
+        result_df = pd.eval("size_score = (result_df[fileName1] * result_df[fileName2])/max_score", target=result_df)
+
+        small_freq = result_df.query("size_score < 0.01").index.tolist()
+        result_df.loc[small_freq,["size_score"]] = 0.01
+
+        # result_df["size_score"] = result_df["size_score"].replace("<0.01",0.01)
+        # print(result_df)
+        # result_df["size_score"] = result_df["size_score"].fillna(0.01)
+        # print(result_df)
+
+        # t1 = time.time()
+        # print(t1 - t0)
+
+        # for i in range(0, len(result_df)):
+        #     score = result_df.iloc[i][fileName1] * result_df.iloc[i][fileName2]
+        #     score = score / max_score
+        #     if score < 0.01:
+        #         score = 0.01
+        #     size_score.append(score)
+
+        # result_df["size_score"] = size_score
+        size_score = result_df["size_score"].tolist()
 
         return [result_df, label, [fileName1, fileName2], size_score]
