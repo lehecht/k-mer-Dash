b84274db317879380b9a39325111606ffd7e3536
Leane <lhecht@techfak.uni-bielefeld.de>
2021-05-27 19:41:33 +0200

    added structural fasta files to settings refs #60

--- src/setting.py
+++ src/setting.py
@@ -1,18 +1,20 @@
 class Setting:
     data = None  # data: file input list
     selected = None  # selected: two files, which are processed
+    secStruct_data = None
     feature = None  # feature: number of T or kmer-Frequency for pcas
     k = 0  # k: kmer length
     peak = 0  # peak: peak: peak-position, where sequences should be aligned
     top = 0  # top: number of best values
 
-    def __init__(self, data, selected, k, peak, top, feature):
+    def __init__(self, data, selected, secStruct_data, k, peak, top, feature):
         self.data = data
         self.selected = selected
         self.k = k
         self.peak = peak
         self.top = top
         self.feature = feature
+        self.secStruct_data = secStruct_data
 
     def setData(self, data):
         self.data = data
@@ -37,6 +39,9 @@ class Setting:
 
     def getData(self):
         return self.data
+
+    def getSecStrucData(self):
+        return self.secStruct_data
 
     def getSelected(self):
         return self.selected
